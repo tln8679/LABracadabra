db485489dc6c47836b7be011da9bda4a7d579805
taylor <tln8679@uncw.edu>
2018-02-08 13:18:31 -0500

    Mad a new activity for the fruit module and created its start upon the fruits button being clicked in the main activity

--- app/src/main/java/com/me/labracadabra/MainActivity.java
+++ app/src/main/java/com/me/labracadabra/MainActivity.java
@@ -1,6 +1,7 @@
 package com.me.labracadabra;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AlertDialog;
@@ -9,7 +10,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,11 +27,26 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu,menu);
 
         //Change menu icon color
-        Drawable icon = menu.getItem(0).getIcon();
-        icon.mutate();
-        icon.setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_IN);
+      //  Drawable icon = menu.getItem(0).getIcon();
+//        icon.mutate();
+     //   icon.setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_IN);
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+
+    /** Called when the user taps Fruit Module */
+    public void openFruits(View view) {
+        Intent intent = new Intent(this, FruitActivity.class);
+       // EditText editText = (EditText) findViewById(R.id.editText);
+       // String message = editText.getText().toString();
+       // intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    /** Called when the user taps Vegetable Module */
+    public void vegBtnClick(View view){
+
     }
 
 
